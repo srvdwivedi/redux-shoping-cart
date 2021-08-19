6b3308fdcaaa33f03424222b320f5fdbba996591
sourav dwivedi <souravdwivedi999@gmail.com>
2021-08-19 16:06:37 +0530

    Add/Remove items in shopping cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,22 @@
 import Card from '../UI/Card';
+import { useSelector } from 'react-redux';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
+
 const Cart = (props) => {
+  const cart = useSelector(state => state.cart.items)
+  console.log(cart)
   return (
     <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
+      <h2> Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cart.map((item) => (
+          <CartItem key={item.id}
+            item={{ title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price, id : item.id }}
+          />
+        ))
+        }
       </ul>
     </Card>
   );
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,14 +2,14 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const DUMMY_PRODUCTS = [
-  {id : 'p1', price : 6, title : 'My First Book', description : 'This is test book.'},
-  {id : 'p2', price : 12, title : 'My Second Book', description : 'This is test book.'}
+  {id : 'p1', price : 6, title : 'How to stay Happy', description : 'Just to start...'},
+  {id : 'p2', price : 12, title : 'How to stay Happy Part-2', description : 'The second book to test'}
 ];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
+      <h2>Products</h2>
       <ul>
           {DUMMY_PRODUCTS.map( (product) => {
             return (
